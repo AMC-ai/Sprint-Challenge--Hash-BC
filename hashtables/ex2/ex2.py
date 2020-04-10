24181798de29e82674c95c9ec34207fbae9908d2
AMC <53350942+AMC-ai@users.noreply.github.com>
2020-04-10 11:19:29 -0700

    updated notes for hash 2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -18,12 +18,18 @@ def reconstruct_trip(tickets, length):
 
     """
     YOUR CODE HERE
+    iterate through tickets objects and insert into hashtable (flight_path)
     link togeither source and destination tickets to reconstruct the trip starting at none by creating a new array to display them and return the flight path that was taken
     """
+    # iterate through
     for ticket in tickets:
+        # using function to "link the ticket objects together"
         hash_table_insert(hashtable, ticket.source, ticket.destination)
+    # new table to display reconstructed flight path
     flight_path = []
+    # gives us the origin
     new_ticket = hash_table_retrieve(hashtable, 'NONE')
+    # appends destination ticket to source ticket in order set in the new table
     while new_ticket != 'NONE':
         flight_path.append(new_ticket)
         new_ticket = hash_table_retrieve(hashtable, new_ticket)
